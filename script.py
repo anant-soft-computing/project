db461bfea357f9febc477a7d93d38913e5a9022c
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-28 17:47:28 +0530

    minor changes

--- script.py
+++ script.py
@@ -3,9 +3,7 @@ from core.models import Event, EventRegistration, Registration
 
 def main():
     for i in Registration.objects.all():
-        print(i.id)
         for j in Event.objects.all():
-            print(j.event_name)
             if not EventRegistration.objects.filter(event=j, registration=i).exists():
                 event_obj = EventRegistration()
                 event_obj.event = j

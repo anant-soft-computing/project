d1477831ffcf3e149fa715c22fb03966fe2be8f6
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-28 12:22:08 +0530

    Initial commit

---
+++ core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
+# Register your models here.
+from .models import Event, Registration
+
+
+@admin.register(Registration)
+class RegistrationAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
---
+++ core/models.py
@@ -0,0 +1,62 @@
+from io import BytesIO
+
+import qrcode
+from django.core.files import File
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=255)
+    event_start_date = models.DateField()
+    event_end_date = models.DateField()
+    event_start_time = models.TimeField()
+    event_end_time = models.TimeField()
+    event_location = models.CharField(max_length=255)
+
+    def _str_(self):
+        return self.event_name
+
+
+class Registration(models.Model):
+    class TshirtSize(models.TextChoices):
+        S = "S", "Small"
+        M = "M", "Medium"
+        L = "L", "Large"
+        XL = "XL", "Extra Large"
+        XXL = "XXL", "Extra Extra Large"
+
+    id = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    batch_year = models.IntegerField(null=True, blank=True)
+    mobile_no = models.CharField(max_length=15, null=True, blank=True)
+    tshirt_size = models.CharField(max_length=3, null=True, choices=TshirtSize.choices)
+    events = models.ManyToManyField(Event, blank=True)  # Adding the ManyToManyField
+
+    registration = models.BooleanField(default=False, null=True, blank=True)
+    kit = models.BooleanField(default=False)
+    tshirt = models.BooleanField(default=False)
+    cap = models.BooleanField(default=False)
+    lapel_pin = models.BooleanField(default=False)
+    barcode = models.ImageField(upload_to="images", null=True, blank=True)
+
+    # def _str_(self):
+    #     return self.name
+
+
+@receiver(post_save, sender=Registration)
+def save_qr_code(sender, created, instance, **kwargs):
+    if created:
+        code = qrcode.make(str(instance.id))
+        image_stream = BytesIO()
+        code.save(image_stream, format="PNG")
+        image_stream.seek(0)
+
+        image = File(image_stream, name=f"{instance.id}.png")
+
+        instance.barcode.save(f"{instance.id}.png", image)

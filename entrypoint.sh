4bc94ed62cbee28b2a17287c71d57e45dd69126e
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2024-01-01 20:43:33 +0530

    minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 # !/bin/sh
 # Add the following line before starting your Django application
+./wait-for-it.sh postres_db:5432 --timeout=15 -- echo "PostgreSQL is ready!"
 python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --noinput
